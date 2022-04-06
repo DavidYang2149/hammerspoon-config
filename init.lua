9e64e0faab6dc71d9de6f9ee4aa238181c768085
Davidyang2149 <davidyang2149@gmail.com>
2022-04-06 16:12:43 +0900

    해머스푼 설정을 초기화합니다
    - 리로드 설정
    - 크롬 단축키 설정
    - 터미널 단축키 설정
    - 화면 이동 단축키 설정
    - 한영 전환 설정
    - ESC 설정

---
+++ init.lua
@@ -0,0 +1,48 @@
+hs.alert.show('Hammer Spoon Online')
+hs.hotkey.bind({'option', 'cmd'}, 'R', hs.reload)
+hs.hotkey.bind({'shift', 'option'}, 'C', function()
+    hs.application.launchOrFocus('Google Chrome')
+end)
+hs.hotkey.bind({'shift', 'option'}, 'T', function()
+    hs.application.launchOrFocus('iTerm')
+end)
+hs.hints.hintChars = {'1', '2', '3', '4', 'Q', 'W', 'E', 'R'}
+hs.hotkey.bind({'shift'}, 'F1', hs.hints.windowHints)
+
+-- 화면 이동 함수
+local function move_win(xx, yy, ww, hh)
+    return function()
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local max = win:screen():frame()
+        f.x = max.w * xx
+        f.y = max.h * yy
+        f.w = max.w * ww
+        f.h = max.h * hh
+        win:setFrame(f)
+    end
+end
+
+local mod = {'option', 'shift'}
+hs.hotkey.bind(mod, '1', move_win(0, 0, 1, 1))
+hs.hotkey.bind(mod, '2', move_win(0, 0, 1/2, 1))
+hs.hotkey.bind(mod, '3', move_win(1/2, 0, 1/2, 1))
+hs.hotkey.bind(mod, '4', move_win(0, 0, 1, 1/2))
+hs.hotkey.bind(mod, '5', move_win(0, 1/2, 1, 1/2))
+
+require('modules.inputsource_aurora')
+
+local inputEnglish = "com.apple.keylayout.ABC"
+local esc_bind
+
+function back_to_eng()
+	local inputSource = hs.keycodes.currentSourceID()
+	if not (inputSource == inputEnglish) then
+		hs.keycodes.currentSourceID(inputEnglish)
+	end
+	esc_bind:disable()
+	hs.eventtap.keyStroke({}, 'escape')
+	esc_bind:enable()
+end 
+
+esc_bind = hs.hotkey.new({}, 'escape', back_to_eng):enable()
